9b00b8b418f7f3602e31face08babcf9d9ab205b
SaranThiyagu <sarankumar.tg@gmail.com>
2021-01-02 09:45:05 -0800

    Add files via upload
    Basedlined - Shuttl new design files..

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+
+!(function($) {
+  "use strict";
+  // Init AOS
+  function aos_init() {
+    AOS.init({
+      duration: 1000,
+      easing: "ease-in-out",
+      once: true,
+      mirror: false
+    });
+  }
+  $(window).on('load', function() {
+    aos_init();
+  });
+
+})(jQuery);
